994e1d9a9150c8f05d8e706b41bf9e40e6f9ccbf
frnca <frnca@LAPTOP-PAFG5EVH>
2022-10-20 14:11:57 -0500

    Agregar archivos de proyecto.

---
+++ UpetApi/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using UpetApi.Entidades;
+
+namespace UpetApi
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Users> Users { get; set; }
+
+    }
+}
---
+++ UpetApi/Migrations/20221020190000_Inicial.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UpetApi.Migrations
+{
+    public partial class Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    correo = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    telefono = table.Column<string>(type: "nvarchar(12)", maxLength: 12, nullable: false),
+                    nombre = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    apellido = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    pais = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    estado = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    municipio = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    codigoPostal = table.Column<int>(type: "int", nullable: false),
+                    colonia = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    calle = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    numeroExterior = table.Column<int>(type: "int", nullable: false),
+                    numeroInterior = table.Column<int>(type: "int", nullable: false),
+                    ine = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    password = table.Column<string>(type: "nvarchar(18)", maxLength: 18, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
